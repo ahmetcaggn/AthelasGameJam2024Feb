b1f266e48a5aee47ee8a35473e9be013c43dfb3f
demirhany <demirhanyilal0@gmail.com>
2024-02-23 22:20:55 +0300

    Conflicts are solved and PlayerDash script file is updated

--- Assets/Scripts/PlayerDash.cs
+++ Assets/Scripts/PlayerDash.cs
@@ -9,16 +9,18 @@ public class PlayerDash : MonoBehaviour
     public Rigidbody2D rb;
     private bool isDashing;
     private bool canDash = true;
-    private float dashingPower = 250f;
+    public float dashingPower = 15f;
     //private float dashingPower = 5f; add force using instead of this
-    private float dashingTime = 1f;
+    private float dashingTime = 0.5f;
     private float dashingCooldown = 0.2f;
     private float Horizontal;
+    PlayerMainMovement playerMovement;
     [SerializeField] private TrailRenderer tr;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        playerMovement = GetComponent<PlayerMainMovement>();
     }
     private void Update()
     {
@@ -34,28 +36,32 @@ public class PlayerDash : MonoBehaviour
     }
     private float CurrentDirection()
     {
-        if(Horizontal < 0)
+        if(playerMovement.isFacingRight == true)
         {
-            return -1;
+            return 1;
         }
         else
         {
-            return 1;
+            return -1;
         }
     }
     private IEnumerator Dash()
     {
+        playerMovement.movementMaxSpeed = 24f;
         canDash = false;
         isDashing = true;
         float originalGravity = rb.gravityScale;
         rb.gravityScale = 0f;
-        rb.AddForce(new Vector2(CurrentDirection() * dashingPower, 0), ForceMode2D.Force); 
+        //rb.AddForce(new Vector2(CurrentDirection() * dashingPower, 0), ForceMode2D.Force); 
+        rb.velocity = new Vector2(transform.localScale.x * dashingPower * CurrentDirection(), 0f);
         tr.emitting = true;
         yield return new WaitForSeconds(dashingTime);
+        rb.velocity = Vector3.zero;
         tr.emitting = false;
         rb.gravityScale = originalGravity;
         isDashing = false;
         yield return new WaitForSeconds(dashingCooldown);
         canDash = true;
+        playerMovement.movementMaxSpeed = 5;
     }
 }
